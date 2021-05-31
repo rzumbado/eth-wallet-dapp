d0325ca3ed6691d3a340568fc638f1cc704dbb29
Rafael Zumbado <rzumbado@gmail.com>
2021-05-31 17:50:03 -0600

    creating proj structure

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace ShowMeDaMoney
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             * I only added this project because I like working with VisualStudio rather than VS Code.
+             * You can still use VS Code if you like or any other IDE of your preference. I happen to like
+             * VS Enterprise so.. sue me! LOL
+             */
+        }
+    }
+}
